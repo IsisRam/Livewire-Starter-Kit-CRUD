c042698e600544f2da22417da6944b53e796b1cb
PovilasKorop <povilask007@gmail.com>
2025-03-06 15:54:45 +0200

    Livewire/Laravel CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -19,6 +20,14 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::get('tasks', \App\Livewire\Tasks\Index::class)->name('tasks.index');
+    Route::get('tasks/create', \App\Livewire\Tasks\Create::class)->name('tasks.create');
+    Route::get('tasks/edit/{task}', \App\Livewire\Tasks\Edit::class)->name('tasks.edit');
+
+    Route::resource('laravel/tasks', TaskController::class);
+
+    Route::view('about', 'about')->name('about');
 });
 
 require __DIR__.'/auth.php';
